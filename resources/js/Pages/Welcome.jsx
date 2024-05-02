b64346d24c94cb3eb7e90d0868bbe8f43548f55b
Orlando L Dominici <orlandoldominici@Orlandos-iMac.local>
2024-05-02 09:45:32 -0400

    fixed errors

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -4,22 +4,17 @@ import Navbar from "@/Components/Navbar";
 import Footer from "@/Components/Footer";
 import Features from "@/Components/Features";
 import Pricing from "@/Components/Pricing";
-import { GTMProvider } from "@elgorditosalsero/react-gtm-hook";
 
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
-    const gtmParams = { id: "GTM-KH9XT38H" };
-
     return (
         <>
-            <GTMProvider state={gtmParams}>
-                <Head title="Welcome" />
+            <Head title="Welcome" />
 
-                <Hero />
+            <Hero />
 
-                <Features />
+            <Features />
 
-                <Pricing />
-            </GTMProvider>
+            <Pricing />
         </>
     );
 }
