763ed71863ff25b8d411cfb52040d2a943763081
Orlando L Dominici <orlandoldominici@Orlandos-iMac.local>
2024-05-03 07:58:53 -0400

    added data layer

--- resources/js/Layouts/AppLayout.jsx
+++ resources/js/Layouts/AppLayout.jsx
@@ -4,7 +4,13 @@ import Footer from "../Components/Footer";
 import { GTMProvider } from "@elgorditosalsero/react-gtm-hook";
 
 const AppLayout = ({ auth, children }) => {
-    const gtmParams = { id: "GTM-KH9XT38H" };
+    const gtmParams = {
+        id: "GTM-KH9XT38H",
+        dataLayer: {
+            "Conversion ID": "16551013734",
+            "Conversion label": "rUbyCJepiKwZEObakdQ9",
+        },
+    };
 
     return (
         <>
